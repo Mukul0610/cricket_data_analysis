9560c5525f4059766a39cdc5ec24a6a97ed2f295
Mukul Rai <mukul93028@gmail.com>
2025-03-18 11:22:21 +0530

    batsman

--- app/api/batsman/route.ts
+++ app/api/batsman/route.ts
@@ -55,7 +55,7 @@ async function getCachedStats(team1:string,team2:string, venue:string) {
 
   const stats: StatsMap = {};
   for (const match of matches) {
-    const { batsman_name, batsman_runs, total_balls, date } = match;
+    const { batsman_name, batsman_runs, total_balls } = match;
     if (!stats[batsman_name]) {
       stats[batsman_name] = { total_runs: 0, total_balls: 0, matches: new Set(), last7: [] };
     }
